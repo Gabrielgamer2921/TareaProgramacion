9e9cfc8483ff615949a33e2470c3384c6728ac8b
Gabriel <gabu8de@gmail.com>
2026-09-05 23:02:17 -0300

    Level Pack Demo

--- Assets/Script/PlayerControler.cs
+++ Assets/Script/PlayerControler.cs
@@ -1,5 +1,6 @@
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerControler : MonoBehaviour
 {
@@ -7,11 +8,13 @@ public class PlayerControler : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
     public float gravityScale = 5f;
+    public float rotateSpeed = 5f;
 
     private Vector3 moveDirection;
 
     public CharacterController charController;
     public Camera playerCamera;
+    public GameObject playerModel;
 
     private void Start()
     {
@@ -25,6 +28,7 @@ public class PlayerControler : MonoBehaviour
 
 
         moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
+        moveDirection.Normalize();
         moveDirection = moveDirection * moveSpeed;
         moveDirection.y = yStore;
 
@@ -40,8 +44,15 @@ public class PlayerControler : MonoBehaviour
         charController.Move(moveDirection * Time.deltaTime);
 
         if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw ("Vertical") != 0)
+        {
 
-        transform.rotation = Quaternion.Euler(0f, playerCamera.transform.rotation.eulerAngles.y, 0f);
+            transform.rotation = Quaternion.Euler(0f, playerCamera.transform.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+
+
+
     }
 
 
