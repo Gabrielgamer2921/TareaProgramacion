584aa3ac32b7dad9f660ff0d8169225c97bf190d
Gabriel <gabu8de@gmail.com>
2026-09-21 15:07:08 -0300

    Assets Update

--- Assets/Script/PlayerControler.cs
+++ Assets/Script/PlayerControler.cs
@@ -1,6 +1,4 @@
-using Unity.Collections;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerControler : MonoBehaviour
 {
@@ -33,8 +31,6 @@ public class PlayerControler : MonoBehaviour
     {
         float yStore = moveDirection.y;
 
-
-
         moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
         moveDirection.Normalize();
         moveDirection = moveDirection * moveSpeed;
@@ -42,19 +38,13 @@ public class PlayerControler : MonoBehaviour
 
         if (charController.isGrounded)
         {
-
             moveDirection.y = 0f;
 
             if (Input.GetButtonDown("Jump"))
             {
-
                 moveDirection.y = jumpForce;
-
             }
-
-
         }
-
 
         moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
 
@@ -62,16 +52,14 @@ public class PlayerControler : MonoBehaviour
 
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {
-
             transform.rotation = Quaternion.Euler(0f, playerCamera.transform.rotation.eulerAngles.y, 0f);
             Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
             playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
         }
 
+       
         animator.SetFloat("Speed", Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z));
-
+        animator.SetBool("IsGrounded", charController.isGrounded); 
     }
-
-
 
 }
